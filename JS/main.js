8ffe6d267888a0139ee88a85b01f6464cb5b7296
Kashif Khan <KashifKhnx04@gmail.com>
2023-08-20 12:30:43 +0500

    adding news section and also company logo carousel

--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,8 @@
 // **************** sticky Nav bar ********************
 const primaryNav = document.querySelector(".primary-navigation");
 const navSticky = primaryNav.offsetTop;
-window.addEventListener('scroll', ()=> {
-    if(window.scrollY > navSticky)
+window.addEventListener('scroll', () => {
+    if (window.scrollY > navSticky)
         primaryNav.classList.add("nav-sticky");
     else
         primaryNav.classList.remove("nav-sticky");
@@ -13,9 +13,10 @@ const hamburgerBtn = document.querySelector(".hamburger-btn");
 const hamburgerIcon = hamburgerBtn.querySelector(".hamburger-icon");
 const navLinks = document.querySelector(".nav-links");
 hamburgerBtn.addEventListener("click", () => {
-  hamburgerIcon.classList.toggle("rotate");
+    hamburgerIcon.classList.toggle("rotate");
     navLinks.classList.toggle("mobile-active");
 });
+
 
 const plusBtn = document.querySelectorAll(".plus-btn");
 plusBtn.forEach((btn) => {
@@ -28,14 +29,14 @@ plusBtn.forEach((btn) => {
             }
         });
         const subNav = btn.nextElementSibling;
-        subNav.classList.toggle("active-sub-nav-link"); 
-        if(subNav.classList.contains("active-sub-nav-link")) {
+        subNav.classList.toggle("active-sub-nav-link");
+        if (subNav.classList.contains("active-sub-nav-link")) {
             plus.className = "fa-solid fa-minus";
-            navLinks.style.minHeight=  "82%";
+            navLinks.style.minHeight = "82%";
         }
         else {
             plus.className = "fa-solid fa-plus";
-            navLinks.style.minHeight=  "0%";
+            navLinks.style.minHeight = "0%";
         }
     });
 });
@@ -59,96 +60,100 @@ const searchCloseBtn = document.querySelector(".search-close-btn");
 const searchMenu = document.querySelector(".search-section");
 const searchOpenBtn = document.querySelector(".search-open-btn");
 searchCloseBtn.addEventListener('click', () => {
-    if(searchMenu.classList.contains("search-section-active"))
+    if (searchMenu.classList.contains("search-section-active"))
         searchMenu.classList.remove("search-section-active");
 });
 
 searchOpenBtn.addEventListener('click', () => {
-    if(!searchMenu.classList.contains("search-section-active"))
+    if (!searchMenu.classList.contains("search-section-active"))
         searchMenu.classList.add("search-section-active");
 });
 
 
-//  carousel 
+// ********************** carousel testimonial *********************************
 const CarouselWrapper = document.querySelector(".carousel-wrapper");
 const carousel = document.querySelector(".carousel");
 const carouselFirstCardWidth = carousel.querySelector(".carousel-card").offsetWidth;
-// const CarouselArrowBtns = document.querySelectorAll(".carousel-wrapper i");
 const carouselChildrens = [...carousel.children];
+
+const companyCarouselWrapper = document.querySelector(".company-carousel-wrapper");
+const companyCarousel = document.querySelector(".company-carousel");
+const companyCarouselFirstCardWidth = companyCarousel.querySelector(".company-carousel-card").offsetWidth;
+const companyCarouselChildrens = [...companyCarousel.children];
 
 let isDragging = false, isAutoPlay = true, startX, startScrollLeft, timeoutId;
 
-// Get the number of cards that can fit in the carousel at once
+let cardPerViewCompany = Math.round(companyCarousel.offsetWidth / companyCarouselFirstCardWidth);
 let cardPerView = Math.round(carousel.offsetWidth / carouselFirstCardWidth);
 
-// Insert copies of the last few cards to beginning of carousel for infinite scrolling
 carouselChildrens.slice(-cardPerView).reverse().forEach(card => {
     carousel.insertAdjacentHTML("afterbegin", card.outerHTML);
 });
-
-// Insert copies of the first few cards to end of carousel for infinite scrolling
 carouselChildrens.slice(0, cardPerView).forEach(card => {
     carousel.insertAdjacentHTML("beforeend", card.outerHTML);
 });
 
-// Scroll the carousel at appropriate postition to hide first few duplicate cards on Firefox
+companyCarouselChildrens.slice(-cardPerViewCompany).reverse().forEach(card => {
+    companyCarousel.insertAdjacentHTML("afterbegin", card.outerHTML);
+});
+companyCarouselChildrens.slice(0, cardPerViewCompany).forEach(card => {
+    companyCarousel.insertAdjacentHTML("beforeend", card.outerHTML);
+});
+
 carousel.classList.add("carousel-no-transition");
 carousel.scrollLeft = carousel.offsetWidth;
 carousel.classList.remove("carousel-no-transition");
 
-// // Add event listeners for the arrow buttons to scroll the carousel left and right
-// CarouselArrowBtns.forEach(btn => {
-//     btn.addEventListener("click", () => {
-//         carousel.scrollLeft += btn.id == "left-carousel-btn" ? -carouselFirstCardWidth : carouselFirstCardWidth;
-//     });
-// });
+companyCarousel.classList.add("carousel-no-transition");
+companyCarousel.scrollLeft = companyCarousel.offsetWidth;
+companyCarousel.classList.remove("carousel-no-transition");
 
 const dragStart = (e) => {
     isDragging = true;
     carousel.classList.add("carousel-dragging");
-    // Records the initial cursor and scroll position of the carousel
+    companyCarousel.classList.add("carousel-dragging");
     startX = e.pageX;
     startScrollLeft = carousel.scrollLeft;
+    startScrollLeft = companyCarousel.scrollLeft;
 }
 
 const dragging = (e) => {
-    if(!isDragging) return; // if isDragging is false return from here
-    // Updates the scroll position of the carousel based on the cursor movement
+    if (!isDragging) return;
     carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+    companyCarousel.scrollLeft = startScrollLeft - (e.pageX - startX);
 }
-
 const dragStop = () => {
     isDragging = false;
     carousel.classList.remove("carousel-dragging");
+    companyCarousel.classList.remove("carousel-dragging");
 }
 
 const infiniteScroll = () => {
-    // If the carousel is at the beginning, scroll to the end
-    if(carousel.scrollLeft === 0) {
+    if (carousel.scrollLeft === 0 || companyCarousel.scrollLeft === 0) {
         carousel.classList.add("carousel-no-transition");
         carousel.scrollLeft = carousel.scrollWidth - (2 * carousel.offsetWidth);
         carousel.classList.remove("carousel-no-transition");
+        companyCarousel.classList.add("carousel-no-transition");
+        companyCarousel.scrollLeft = companyCarousel.scrollWidth - (2 * companyCarousel.offsetWidth);
+        companyCarousel.classList.remove("carousel-no-transition");
     }
-    // If the carousel is at the end, scroll to the beginning
-    else if(Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth) {
+    else if (Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth || Math.ceil(companyCarousel.scrollLeft) === companyCarousel.scrollWidth - companyCarousel.offsetWidth) {
         carousel.classList.add("carousel-no-transition");
         carousel.scrollLeft = carousel.offsetWidth;
         carousel.classList.remove("carousel-no-transition");
+        companyCarousel.classList.add("carousel-no-transition");
+        companyCarousel.scrollLeft = companyCarousel.offsetWidth;
+        companyCarousel.classList.remove("carousel-no-transition");
     }
-
-    // Clear existing timeout & start autoplay if mouse is not hovering over carousel
     clearTimeout(timeoutId);
-    if(!CarouselWrapper.matches(":hover")) autoPlay();
+    if (!CarouselWrapper.matches(":hover") || !companyCarouselWrapper.matches(":hover")) autoPlay();
 }
-
 const autoPlay = () => {
-    // if(window.innerWidth < 800 || !isAutoPlay) return; // Return if window is smaller than 800 or isAutoPlay is false
-    if(!isAutoPlay) return; // Return if window is smaller than 800 or isAutoPlay is false
-    // Autoplay the carousel after every 2500 ms
     timeoutId = setTimeout(() => carousel.scrollLeft += carouselFirstCardWidth, 5000);
+    timeoutId = setTimeout(() => companyCarousel.scrollLeft += companyCarouselFirstCardWidth, 4500);
 }
-autoPlay();
 
+autoPlay();
 carousel.addEventListener("mousedown", dragStart);
 carousel.addEventListener("mousemove", dragging);
 document.addEventListener("mouseup", dragStop);
@@ -156,18 +161,25 @@ carousel.addEventListener("scroll", infiniteScroll);
 CarouselWrapper.addEventListener("mouseenter", () => clearTimeout(timeoutId));
 CarouselWrapper.addEventListener("mouseleave", autoPlay);
 
-// Back to top button
+companyCarousel.addEventListener("mousedown", dragStart);
+companyCarousel.addEventListener("mousemove", dragging);
+document.addEventListener("mouseup", dragStop);
+companyCarousel.addEventListener("scroll", infiniteScroll);
+companyCarouselWrapper.addEventListener("mouseenter", () => clearTimeout(timeoutId));
+companyCarouselWrapper.addEventListener("mouseleave", autoPlay);
+
+// ********************** Scroll to Top *********************************
 const scrollTopBtn = document.querySelector(".scroll-to-top")
 
-window.addEventListener('scroll', ()=> {
-    if(window.scrollY > 300)
-    scrollTopBtn.style.display = "block"
-    else 
-    scrollTopBtn.style.display = "none"
-        
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 300)
+        scrollTopBtn.style.display = "block"
+    else
+        scrollTopBtn.style.display = "none"
+
 });
 
-scrollTopBtn.addEventListener('click', ()=> {
+scrollTopBtn.addEventListener('click', () => {
     window.scrollTo({
         top: 0,
         behavior: "smooth"
